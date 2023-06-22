feade0ea0816b05de20136a42fef73151f055a32
prankshadow <padmesh.sharma26@gmail.com>
2023-06-22 14:38:32 +0530

    created form page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
+import { useSession } from 'next-auth/react'; //To know which user is currentlt login
+import { useRouter } from 'next/router';
+
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
-  return (
-    <div>CreatePrompt</div>
-  )
+
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: "",
+        tag: "",
+    });
+
+    const createPrompt = async (e) => {
+
+    }
+
+    return (
+        <Form
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
+    )
 }
 
 export default CreatePrompt
