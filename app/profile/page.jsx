cd9e99e87f488b077a9f3ec540ad8a3d80428427
prankshadow <padmesh.sharma26@gmail.com>
2023-06-24 16:09:41 +0530

    created Prompt card

---
+++ app/profile/page.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Profile from '@components/Profile';
+
+const MyProfile = () => {
+    return (
+        <Profile />
+    )
+}
+
+export default MyProfile
