1e4bcd692f1bc7d33cb994e116c97915d8d8f46d
prankshadow <padmesh.sharma26@gmail.com>
2023-06-29 18:09:33 +0530

    done with edit functionaly

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/navigation';
 import Profile from '@components/Profile';
 
 const MyProfile = () => {
+    const router = useRouter();
 
     const { data: session } = useSession();
 
@@ -22,13 +23,13 @@ const MyProfile = () => {
 
 
         if (session?.user.id) fetchPosts();
-    }, [])
+    }, [session?.user.id])
 
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
     }
 
-    const handleDelete = () => {
+    const handleDelete = (post) => {
 
     }
 
--- components/Form.jsx
+++ components/Form.jsx
@@ -8,7 +8,7 @@ const Form = ({
     <section className='w-full max-w-full flex-start flex-col'>
       <h1 className='head_text text-left'><span className='blue_gradient'>{type} Posts</span></h1>
       <p className='desc text-left max-w-md'>
-        {type} and share amazindg promps with he world, and let your imagination run wild with any AI-powered platform
+        {type} and share amazing promps with he world, and let your imagination run wild with any AI-powered platform
       </p>
 
       <form onSubmit={handleSubmit} className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'>
